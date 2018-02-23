b15e18fff2c1b2ec3e340570b26211191fe8133f
Taylor Liss <tliss@camsys.com>
2018-02-23 13:35:15 -0500

    first commit

---
+++ desktop/src/com/tayloraliss/liftlanders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tayloraliss.liftlanders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tayloraliss.liftlanders.LiftLandersGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LiftLandersGame(), config);
+	}
+}
