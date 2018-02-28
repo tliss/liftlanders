a35a0656e9efb8952dcb566df5629135e309acf1
Taylor Liss <tliss@camsys.com>
2018-02-28 15:51:37 -0500

    Added lifts and positioned them correctly

---
+++ core/src/com/tayloraliss/liftlanders/Lift.java
@@ -0,0 +1,17 @@
+package com.tayloraliss.liftlanders;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Lift extends BaseActor {
+
+    public Lift(float x, float y, Stage s)
+    {
+        super(x,y,s);
+        loadTexture("lift.png");
+    }
+
+    public void act(float dt)
+    {
+
+    }
+}
--- core/src/com/tayloraliss/liftlanders/LiftLanders.java
+++ core/src/com/tayloraliss/liftlanders/LiftLanders.java
@@ -1,12 +1,19 @@
 package com.tayloraliss.liftlanders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.compression.lzma.Base;
 
 public class LiftLanders extends GameBeta {
 
 	// Declare actors here
 	private GreenBot greenbot;
+	private Lift liftLeft;
+	private Lift liftRight;
+
 	private static final int PLATFORM_TOTAL = 12;
+	private static final int PLATFORM_WIDTH = 182;
+	private static final int PLATFORM_START_HEIGHT = 400;
+	private static final int PLATFORM_END_HEIGHT = PLATFORM_START_HEIGHT - 200;
 
 	public void initialize(){
 		// Initialize actors here
@@ -14,18 +21,23 @@ public class LiftLanders extends GameBeta {
 		greenbot.moveBy(-greenbot.getWidth()/2,0);
 
 		for (int i = 0; i <= PLATFORM_TOTAL-1; i++) {
-			for(int y = 500; y >= 300; y -= 100) {
-				for (int x = 0; x < Gdx.graphics.getWidth(); x += 182 * 2) {
+			for(int y = PLATFORM_START_HEIGHT; y >= PLATFORM_END_HEIGHT; y -= 100) {
+				for (int x = 0; x < Gdx.graphics.getWidth(); x += PLATFORM_WIDTH * 2) {
 					new Platform(x, y, mainStage);
 				}
 			}
 		}
+
+		liftLeft = new Lift(PLATFORM_WIDTH, 400, mainStage);
+		liftRight = new Lift(PLATFORM_WIDTH * 3, 200, mainStage);
 
 	}
 
 	public void update(float dt){
 		for (BaseActor platform : BaseActor.getList(mainStage, "Platform"))
 			greenbot.preventOverlap(platform);
+		for (BaseActor lift : BaseActor.getList(mainStage, "Lift"))
+			greenbot.preventOverlap(lift);
 	}
 
 }
